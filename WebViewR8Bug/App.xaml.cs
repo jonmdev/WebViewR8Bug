4c94def20a7e469839e1f198e8d6d0f1a2422340
jonmdev <43948450+jonmdev@users.noreply.github.com>
2024-06-13 01:46:13 -0400

    Initial commit

---
+++ WebViewR8Bug/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace WebViewR8Bug {
+    public partial class App : Application {
+        public App() {
+
+            ContentPage mainPage = new();
+            this.MainPage = mainPage;
+
+            AbsoluteLayout absRoot = new();
+            mainPage.Content = absRoot;
+
+            WebView webView = new();
+            webView.WidthRequest = 300; 
+            webView.HeightRequest = 300;
+            webView.Source = "https://google.com";
+            
+            absRoot.Add(webView);
+        }
+    }
+}
